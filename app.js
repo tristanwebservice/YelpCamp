ea58c61ef09edf5384b06ed4733cf3a243330307
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-07-16 12:26:59 +0200

    Chore: Install ejs-mate for layout templating

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require(`path`);
 const mongoose = require(`mongoose`);
 const Campground = require(`./models/campground`);
 const methodOverride = require(`method-override`);
+const ejsMate = require(`ejs-mate`);
 
 mongoose.connect(`mongodb://localhost:27017/yelp-camp`); // mongo listening on 27017, temporary hardcode
 
@@ -15,6 +16,7 @@ db.once(`open`, () => {
 
 const app = express();
 
+app.engine(`ejs`, ejsMate);
 app.set(`view engine`, `ejs`); // ejs for dynamic web pages
 app.set(`views`, path.join(__dirname, `views`));
 
