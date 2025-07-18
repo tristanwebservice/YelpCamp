e421c205ece55ebcbb60c338b9e78bfae65e6b4e
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-07-18 13:07:54 +0200

    feat: Add image and description seeding to articles

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea quibusdam, expedita dolorem repudiandae assumenda atque ullam aliquam iusto cumque neque qui amet hic tempora doloremque necessitatibus, delectus harum nihil libero?`,
+      price,
     });
     await camp.save();
   }
