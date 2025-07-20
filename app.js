514b782af6eaf1a79974e9f0708346367b0078c3
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-07-20 09:22:09 +0200

    feat: Implement basic error handling middleware and 404 catch-all

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require(`mongoose`);
 const Campground = require(`./models/campground`);
 const methodOverride = require(`method-override`);
 const ejsMate = require(`ejs-mate`);
+const catchAsync = require(`./utils/catchAsync`);
+const ExpressError = require(`./utils/ExpressError`);
 
 mongoose.connect(`mongodb://localhost:27017/yelp-camp`); // mongo listening on 27017, temporary hardcode
 
@@ -30,10 +32,13 @@ app.get(`/`, (req, res) => {
 });
 
 // read: displays all campgrounds
-app.get(`/campgrounds`, async (req, res) => {
-  const campgrounds = await Campground.find({}); // fetches data from db
-  res.render(`campgrounds/index`, { campgrounds }); // render index.ejs and pass data to it
-});
+app.get(
+  `/campgrounds`,
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({}); // fetches data from db
+    res.render(`campgrounds/index`, { campgrounds }); // render index.ejs and pass data to it
+  })
+);
 
 // create show form new campground
 app.get(`/campgrounds/new`, (req, res) => {
@@ -41,37 +46,65 @@ app.get(`/campgrounds/new`, (req, res) => {
 });
 
 // create, new submission
-app.post(`/campgrounds`, async (req, res) => {
-  const campground = new Campground(req.body.campground);
-  await campground.save();
-  res.redirect(`/campgrounds/${campground._id}`);
-});
+app.post(
+  `/campgrounds`,
+  catchAsync(async (req, res, next) => {
+    if (!req.body.campground)
+      throw new ExpressError(`Invalid Campground Data`, 400);
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
 // read, display details
-app.get(`/campgrounds/:id`, async (req, res) => {
-  const campground = await Campground.findById(req.params.id); // error handling missing
-  res.render(`campgrounds/show`, { campground });
-});
+app.get(
+  `/campgrounds/:id`,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id); // error handling missing
+    res.render(`campgrounds/show`, { campground });
+  })
+);
 
 // update: edit a campground
-app.get(`/campgrounds/:id/edit`, async (req, res) => {
-  const campground = await Campground.findById(req.params.id); // error handling missing
-  res.render(`campgrounds/edit`, { campground });
-});
+app.get(
+  `/campgrounds/:id/edit`,
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id); // error handling missing
+    res.render(`campgrounds/edit`, { campground });
+  })
+);
 
 // update: handling submissoin
-app.put(`/campgrounds/:id`, async (req, res) => {
-  const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {
-    ...req.body.campground,
-  });
-  res.redirect(`/campgrounds/${campground.id}`);
+app.put(
+  `/campgrounds/:id`,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground.id}`);
+  })
+);
+
+app.delete(
+  `/campgrounds/:id`,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect(`/campgrounds`);
+  })
+);
+
+// error handler
+app.all(/(.*)/, (req, res, next) => {
+  next(new ExpressError(`Page Not Found`, 404));
 });
 
-app.delete(`/campgrounds/:id`, async (req, res) => {
-  const { id } = req.params;
-  await Campground.findByIdAndDelete(id);
-  res.redirect(`/campgrounds`);
+// error handler
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = `Something went wrong!` } = err;
+  res.status(statusCode).send(message);
 });
 
 // start the express web app, listening for requests
