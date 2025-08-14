b8d4ff54d2b7798a97b311f46a43073cd869113d
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-08-14 12:55:55 +0200

    Add Joi validation to campground creation route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require(`method-override`);
 const ejsMate = require(`ejs-mate`);
 const catchAsync = require(`./utils/catchAsync`);
 const ExpressError = require(`./utils/ExpressError`);
-
+const Joi = require(`joi`);
 mongoose.connect(`mongodb://localhost:27017/yelp-camp`); // mongo listening on 27017, temporary hardcode
 
 // error handling for db connection
@@ -45,12 +45,26 @@ app.get(`/campgrounds/new`, (req, res) => {
   res.render(`campgrounds/new`);
 });
 
-// create, new submission
+// create, new submission, validate required fields with joi
 app.post(
   `/campgrounds`,
   catchAsync(async (req, res, next) => {
     if (!req.body.campground)
       throw new ExpressError(`Invalid Campground Data`, 400);
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required(),
+      }).required(),
+    });
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(`,`);
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
