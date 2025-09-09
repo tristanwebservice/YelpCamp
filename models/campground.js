57f4eb9515817d0e58b98c00894866dd9465dd5e
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-09-09 17:10:25 +0200

    refactor: Add middleware to delete associated reviews with campground

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,7 @@
 const mongoose = require(`mongoose`); // mongoose lib
+const { campgroundSchema } = require("../schemas");
 const Schema = mongoose.Schema; // shortcut
+const Review = require(`./review`);
 
 // the blueprint for each campground
 const CampgroundSchema = new Schema({
@@ -14,6 +16,12 @@ const CampgroundSchema = new Schema({
       ref: `Review`,
     },
   ],
+});
+
+CampgroundSchema.post(`findOneAndDelete`, async function (doc) {
+  if (doc) {
+    await Review.deleteMany({ _id: { $in: doc.reviews } });
+  }
 });
 
 // mongoose will create a model named Campground based on the schema
