95ed31d4fad5a774d821eb596d7eb48dc12c44ff
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-09-11 22:30:10 +0200

    chore: Organize form validation client-side scripts in public folder

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, `public`)));
 
 // routes
 app.use("/campgrounds", campgrounds);
---
+++ public/js/validateForms.js
@@ -0,0 +1,21 @@
+(function () {
+  "use strict";
+  // selects all forms that will use bootstrap validation
+  const forms = document.querySelectorAll(".validated-form");
+
+  // loop over the forms and prevent submit
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
