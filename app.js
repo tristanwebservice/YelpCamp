a395830742dc6eec435a1bdea9754ad3c6cfd86e
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-09-11 22:52:14 +0200

    chore: Implement and configure user session management

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const session = require(`express-session`);
 const ExpressError = require("./utils/ExpressError");
 
 const campgrounds = require("./routes/campgrounds");
@@ -25,6 +26,19 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, `public`)));
+
+const sessionConfig = {
+  secret: `secret`,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
+};
+
+app.use(session(sessionConfig));
 
 // routes
 app.use("/campgrounds", campgrounds);
