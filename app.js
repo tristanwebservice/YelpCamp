fd91c11d6531be70970f60a8875bc5378842613d
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-09-13 13:36:07 +0200

    feat: Configure connect-flash for messaging

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require(`express-session`);
 const ExpressError = require("./utils/ExpressError");
+const flash = require(`connect-flash`);
 
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
@@ -39,6 +40,13 @@ const sessionConfig = {
 };
 
 app.use(session(sessionConfig));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash(`success`);
+  res.locals.error = req.flash(`error`);
+  next();
+});
 
 // routes
 app.use("/campgrounds", campgrounds);
