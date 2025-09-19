759b2dc12baea6f233b91249b358bc4decf83ca4
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-09-19 16:04:32 +0200

    feat: Implement permissions for campground ownership

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -66,7 +66,16 @@ router.get(
   "/:id/edit",
   isLoggedIn,
   catchAsync(async (req, res) => {
+    const { id } = req.params;
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash(`error`, `Cannot find that campground`);
+      return res.redirect(`/campgrounds/`);
+    }
+    if (!campground.author.equals(req.user._id)) {
+      req.flash(`error`, `You do not have permission to do that`);
+      return res.redirect(`/campgrounds/${id}`);
+    }
     res.render("campgrounds/edit", { campground });
   })
 );
@@ -78,7 +87,13 @@ router.put(
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
+    const campground = await Campground.findById(id);
+
+    if (!campground.author.equals(req.user._id)) {
+      req.flash(`error`, `You do not have permission to do that`);
+      return res.redirect(`/campgrounds/${id}`);
+    }
+    const camp = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
     req.flash(`success`, `Successfully updated campground!`);
