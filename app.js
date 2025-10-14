8eac2830b232595d7621982116e2c74a7f34056c
tristanwebservice <198068068+tristanwebservice@users.noreply.github.com>
2025-10-14 12:22:10 +0200

    refactor: Dynamically set DB URL for dev/prod

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ const { contentSecurityPolicy } = require("helmet");
 
 const MongoStore = require("connect-mongo");
 
-// const dbUrl = process.env.DB_URL;
-const dbUrl = "mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || `mongodb://localhost:27017/yelp-camp`;
 
 mongoose.connect(dbUrl);
 
